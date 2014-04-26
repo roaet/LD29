999dc4cba114751b3bbab8f7741aea51ff8f6624
Justin Hammond <runningempty@gmail.com>
2014-04-25 23:41:10 -0500

    Fixed the damned ignore

---
+++ unity/Assets/scripts/tests/TestLevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// this class is used to test the level loader
+public class TestLevelLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
