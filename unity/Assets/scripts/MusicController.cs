2a27cdeeeeef9b038044f3e8b24c66146168ac1c
Justin Hammond <runningempty@gmail.com>
2014-04-27 01:48:38 -0500

    Maybe fixes

---
+++ unity/Assets/scripts/MusicController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+	public Sprite noteOn;
+	public Sprite noteOff;
+
+	public AudioSource musicSource;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp() {
+
+	}
+}
